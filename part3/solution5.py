fbe54e915a7cf8f92c47a5665f4ea73783cadff9
username <username>
2022-10-24 23:27:02 +0700

    для решения задачи 5, части 3 был добавлен случай, когда высота коврика - четное число

--- part3/solution5.py
+++ part3/solution5.py
@@ -20,34 +20,57 @@ def solution():
     height = 11
     width = height * 3
 
-    tree_segment = int(height * 0.3)
+    tree_segment1 = int(height * 0.25)
+    tree_segment2 = int(height * 0.45)
 
-    print("<>" + "TUSUR".center(width - 4, "=") + "<>")
+    print("<>" + "=" * (width - 4) + "<>")
 
-    for line_index in range(1, tree_segment + 1):
-        print(
-            "||" + 
-            ("/" + "#" * (line_index - 1)).rjust(width // 2 - 2, "-") +
-            "#" +
-            ("#" * (line_index - 1) + "\\").ljust(width // 2 - 2, "-") +
-            "||"
-        )
+    if height % 2 != 0:
+        for line_index in range(1, tree_segment1 + 1):
+            print(
+                "||" + 
+                ("/" + "#" * (line_index - 1)).rjust(width // 2 - 2, "-") +
+                "#" +
+                ("#" * (line_index - 1) + "\\").ljust(width // 2 - 2, "-") +
+                "||"
+            )
 
-    print("||" + "|||".center(width - 4, "-") + "||")
-
-    for line_index in range(1, tree_segment + 1):
-        print(
-            "||" +
-            ("/" + "#" * (line_index + 2)).rjust(width // 2 - 2, "-") +
-            "#" +
-            ("#" * (line_index + 2) + "\\").ljust(width // 2 - 2, "-") +
-            "||"
-        )
-
-    for line_index in range(2):
         print("||" + "|||".center(width - 4, "-") + "||")
 
-    print("<>" + "TUSUR".center(width - 4, "=") + "<>")
+        for line_index in range(1, tree_segment2 + 1):
+            print(
+                "||" +
+                ("/" + "#" * (line_index + 2)).rjust(width // 2 - 2, "-") +
+                "#" +
+                ("#" * (line_index + 2) + "\\").ljust(width // 2 - 2, "-") +
+                "||"
+            )
+
+        for line_index in range(2):
+            print("||" + "|||".center(width - 4, "-") + "||")
+    else:
+        for line_index in range(1, tree_segment1 + 1):
+            print(
+                "||" + 
+                ("/" + "#" * (line_index - 1)).rjust(width // 2 - 2, "-") +
+                ("#" * (line_index - 1) + "\\").ljust(width // 2 - 2, "-") +
+                "||"
+            )
+
+        print("||" + "||".center(width - 4, "-") + "||")
+
+        for line_index in range(1, tree_segment2 + 1):
+            print(
+                "||" +
+                ("/" + "#" * (line_index + 2)).rjust(width // 2 - 2, "-") +
+                ("#" * (line_index + 2) + "\\").ljust(width // 2 - 2, "-") +
+                "||"
+            )
+
+        for line_index in range(2):
+            print("||" + "||".center(width - 4, "-") + "||")
+
+    print("<>" + "=" * (width - 4) + "<>")
 
 
 if __name__ == "__main__":
