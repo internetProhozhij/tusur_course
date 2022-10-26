8d7e6493215046e172fa6dd2c9612891593c0e62
user <user>
2022-10-26 10:35:39 +0700

    >> Добавлено решение 6-ого задания 3-ей части

---
+++ part3/solution6.py
@@ -0,0 +1,35 @@
+"""
+6. Произведение цифр
+
+Дано: целое число.
+ 
+Задание: 
+    написать программу, которая перемножит все цифры заданного
+    числа (0 - исключить).
+ 
+ 
+Примеры: 
+    1) value = 123405, результат = 120
+    2) value = 999, результат = 729
+    3) value = 1000, результат = 1
+    4) value = 1111, результат = 1
+"""
+
+
+def solution(number) -> int:
+    step = 10
+    answer = 1
+
+    while number > 0:
+        digit = number % 10
+        if digit != 0:
+            answer *= digit
+        number //= step
+
+    return answer 
+
+
+if __name__ == "__main__":
+    number = 0
+    answer = solution(number)
+    print(answer)
