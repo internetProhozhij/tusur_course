d3a62c1d610ffb6bcf0fc5693de964781d871bd2
user <user>
2022-11-17 18:13:50 +0700

    Добавлено решение третьего задания седьмой части

---
+++ part7/solution3.py
@@ -0,0 +1,31 @@
+"""
+3. Ленивый спекулянт
+
+Дано: 
+    словарь банк - курс доллара (dict).
+
+Задание: 
+    определить банк и курс покупки валюты с наиболее привлекательным предложением.
+
+Пример:
+    rates = {'Sberbank': 55.8, 'VTB24': 53.91}, результат: VTB24 -> 53.91 
+"""
+
+
+def solution(rates: dict[str, float]) -> str:
+    rates_items = list(rates.items())
+
+    target = min(rates_items, key=lambda pair: pair[1])
+
+    return f"{target[0]} -> {target[1]}"
+    
+
+if __name__ == "__main__":
+    rates = {
+        "СБЕР": 55.8,
+        "ВТБ": 53.91,
+        "Альфа": 47.57,
+    }
+
+    answer = solution(rates)
+    print(answer)
